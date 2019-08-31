64f3de66c2e467dc3e5db116875d08d5c6c6f271
Natan Bobrov <alexander.andreev@me.com>
2019-09-01 01:18:28 +0300

    Update ru.js
    добавил новые фразы

--- public/js/lang/ru.js
+++ public/js/lang/ru.js
@@ -385,6 +385,10 @@ window.appLang = {
     "node.geo.cn": "Азия",
     "node.geo.undefined": "не определено",
 
+    "node.type.Proxy": "Прокси",
+    "node.type.InfrastructureTorrent": "Инфраструктурный торрент",
+    "node.delegation": ["делегирование", "делегирования", "делегирований"],
+
     // NodeView
     "nodeview.error.notenoghfunds": "Недостаточно средств для делегирования",
     "nodeview.filter.recommended": "Рекомендуемые",
@@ -471,3 +475,14 @@ window.timeToDateTime = function (time, date) {
 
     return (date ? dateStr + " " : "") + timeStr;
 };
+
+
+/**
+ * @param  {integer} count quantity for word
+ * @param  {array} words array of words. Example: ['кошелек', 'кошелька', 'кошельков'], ['делегирование', 'делегирования', 'делегирований']
+ * @return {string} count + plural form for word
+ */
+window.pluralize = function (count, words) {
+    var cases = [2, 0, 1, 1, 1, 2];
+    return count + ' ' + words[ (count % 100 > 4 && count % 100 < 20) ? 2 : cases[ Math.min(count % 10, 5)] ];
+}
